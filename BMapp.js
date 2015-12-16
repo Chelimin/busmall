373840e07e9f82da6b1db962409a5d7090fb1f24
He Limin <chelimin@gmail.com>
2015-12-16 10:22:30 -0800

    changed busmall to render barchart for results

--- BMapp.js
+++ BMapp.js
@@ -27,15 +27,15 @@ var chair = new Product("chair","BMimgs/chair.jpg");
 var scissors = new Product("scissors","BMimgs/scissors.jpg");
 var water_can = new Product("water_can","BMimgs/water_can.jpg");
 var wine_glass = new Product("wine_glass","BMimgs/wine_glass.jpg");
-var bag = new Product("boots","BMimgs/bag.jpg");
-var banana = new Product("boots","BMimgs/banana.jpg");
-var cthulhu = new Product("boots","BMimgs/cthulhu.jpg");
-var dragon = new Product("boots","BMimgs/dragon.jpg");
-var pen = new Product("boots","BMimgs/pen.jpg");
-var shark = new Product("boots","BMimgs/shark.jpg");
-var sweep = new Product("boots","BMimgs/sweep.png");
-var unicorn = new Product("boots","BMimgs/unicorn.jpg");
-var usb = new Product("boots","BMimgs/usb.gif");
+var bag = new Product("bag","BMimgs/bag.jpg");
+var banana = new Product("banana","BMimgs/banana.jpg");
+var cthulhu = new Product("cthulhu","BMimgs/cthulhu.jpg");
+var dragon = new Product("dragon","BMimgs/dragon.jpg");
+var pen = new Product("pen","BMimgs/pen.jpg");
+var shark = new Product("shark","BMimgs/shark.jpg");
+var sweep = new Product("sweep","BMimgs/sweep.png");
+var unicorn = new Product("unicorn","BMimgs/unicorn.jpg");
+var usb = new Product("usb","BMimgs/usb.gif");
 
 // function buildAlbum(){
 //   for (var i=0; i < allProducts.length; i++){
@@ -83,22 +83,24 @@ var productRank = {
       productRank.rightEl.src = productRank.rightObj.path;
       productRank.rightEl.id = productRank.rightObj.name;
 
-},
+    },
 
-  showResults: function(){
-  if (productRank.totalClicks % 15 === 0){
-    productRank.resultsEl.hidden = false;
-    // this.resultsButton.addEventListner('click',function(){
-    //   productRank.resultsButton.hidden=true;
-    //   productRank.barChart = new Chart(productRank.ctx).Bar(data);
-    // });
-  } else {
-    productRank.resultsEl.hidden = true;
-  }
+    showResults: function(){
+      if (productRank.totalClicks % 15 === 0){
+      productRank.resultsEl.hidden = false;
+      // this.resultsButton.addEventListner('click',function(){
+      //   productRank.resultsButton.hidden=true;
+      //   productRank.barChart = new Chart(productRank.ctx).Bar(data);
+      // });
+      } else {
+      productRank.resultsEl.hidden = true;
+        }
+      }
+
+    }
 
 
-
-}
+    // to sort products by clicks >>> non-working code
 
     // arrange: function(){
     //   var tallycount = [];
@@ -120,9 +122,7 @@ var productRank = {
     //           }
     //
     //   tallycount.sort(compare);
-    //
 
-};
 
     productRank.leftEl.addEventListener('click', function(){
     productRank.leftObj.tally +=1;
@@ -130,7 +130,7 @@ var productRank = {
     console.log("This is the new number: " + productRank.leftObj.tally);
     productRank.showResults();
     productRank.displayImages();
-  })
+    })
 
     productRank.midEl.addEventListener('click', function(){
     productRank.midObj.tally +=1;
@@ -138,7 +138,7 @@ var productRank = {
     console.log("This is the new number: " + productRank.midObj.tally);
     productRank.showResults();
     productRank.displayImages();
-  })
+    })
 
     productRank.rightEl.addEventListener('click', function(){
     productRank.rightObj.tally +=1;
@@ -146,18 +146,50 @@ var productRank = {
     console.log("This is the new number: " + productRank.rightObj.tally);
     productRank.showResults();
     productRank.displayImages();
-  })
-
-    var resultList = document.getElementById("list");
-    var logResults = function(){
-      for (var i=0; i < allProducts.length; i++){
-        var li = document.createElement("li");
-        li.textContent = "Total number of clicks for " + allProducts[i].name + " is: "+ allProducts[i].tally;
-        resultList.appendChild(li);
-        }
-      }
-
-      var displayResults = document.getElementById('results');
-      displayResults.addEventListener('click', logResults);
+    })
 
 productRank.displayImages()
+
+    // to log results after 15 clicks >>>working code
+
+    // var resultList = document.getElementById("list");
+    // var logResults = function(){
+    //   for (var i=0; i < allProducts.length; i++){
+    //     var li = document.createElement("li");
+    //     li.textContent = "Total number of clicks for " + allProducts[i].name + " is: "+ allProducts[i].tally;
+    //     resultList.appendChild(li);
+    //     }
+    //   }
+    //
+    var displayResults = document.getElementById('results');
+    displayResults.addEventListener('click', makeChart);
+    //   //command to clear list but doesnt work ?!
+    //   // document.getElementsById('results').innerHTML=""
+//};
+
+
+    // bar chart data
+    function makeChart(){
+            var barData = {
+                  labels : [],
+                  datasets : [
+                      {
+                        fillColor : "#48A497",
+                        strokeColor : "#48A4D1",
+                        data : []
+                      }
+                ]
+            }
+
+            for (var i = 0; i < allProducts.length; i++){
+              barData.datasets[0].data.push(allProducts[i].tally);
+              //there is only one object within the array of datasets
+              barData.labels.push(allProducts[i].name);
+            }
+
+            // get bar chart canvas
+            var ctx = document.getElementById("productChart").getContext("2d");
+            var newChart = new Chart(ctx).Bar(barData);
+
+onClick="window.location.reload(true)"
+}
